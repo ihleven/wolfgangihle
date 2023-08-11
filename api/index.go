98084925b9037696e1dcfbc5f06a3c257aff0f57
Matthias Ihle <matthias.ihle@interhome.group>
2023-08-11 11:35:15 +0200

    go function mit Zugriff auf env

--- api/index.go
+++ api/index.go
@@ -3,8 +3,9 @@ package handler
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+	fmt.Fprintf(w, "<h1>Hello %s from Go!</h1>", os.Getenv("NITRO_PRESET"))
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ihleven/wolfgangihle
+
+go 1.20
