6c241ca309b374ad139b3270b00989c48b107fb2
ihleven <github@ihleven.de>
2023-10-03 15:51:07 +0200

    meta data

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,7 +24,18 @@ export default defineNuxtConfig({
 
   app: {
     head: {
+      title: 'Wolfgang Ihle | Malerei',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Die Webseite des Ortenauer Malers Wolfgang Ihle',
+        },
+      ],
       link: [{ rel: 'icon', type: 'image/jpeg', href: '/icon.jpg' }],
+      // link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
     },
   },
 
